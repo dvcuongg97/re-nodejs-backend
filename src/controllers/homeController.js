ccf2c9f9184c36af9beaa9071b300241ea90f6d9
dvcuong97 <cuonggdv08@gmail.com>
2024-01-21 22:00:21 +0700

    table user: render data

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,18 @@
 // handler function
 const connection = require("../config/database");
-
-const getHomepage = (req, res) => {
-  return res.render("home.ejs");
-};
+const { getAllUser } = require("../services/CRUDService");
 
 const getEjs = (req, res) => {
   res.render("sample.ejs");
 };
 
-const postCreateUser = (req, res) => {
+const getHomepage = async (req, res) => {
+  const listUsers = await getAllUser();
+  console.log(">> check results::", listUsers);
+  return res.render("home.ejs", { listUsers });
+};
+
+const postCreateUser = async (req, res) => {
   const { email, name, city } = req.body;
   console.log(
     ">>check email ::",
@@ -19,17 +22,14 @@ const postCreateUser = (req, res) => {
     ">>check city ::",
     city
   );
-  // Using placeholders
-  connection.query(
+  let [results, fields] = await connection.query(
     `INSERT INTO
     Users  (email, name, city)
     VALUES(?, ? ,?)`,
-    [email, name, city],
-    function (err, results) {
-      console.log(">>check results ::", results);
-      res.send("Create new user succeed !!");
-    }
+    [email, name, city]
   );
+  console.log(">> check results ::", results);
+  res.send("Create new user succeed !!");
 };
 
 const getCreatePage = (req, res) => {
